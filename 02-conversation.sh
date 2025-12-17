e5336c97a764c0204fbec706a02860a10b0bfd1b
Karthik Mallina <cloudbykarthikmallina@gmail.com>
2025-12-17 20:35:26 +0530

    Scripting

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh, How are You?"
+echo "Suresh:: Hello Ramesh, I am fine. How are you?"
+echo "Ramesh:: What are you Learning Reacently?"
+echo "Suresh:: I am Learning Shell Scripting Ramesh"
+
