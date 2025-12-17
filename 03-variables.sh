25e7e7495ae5555f0702b97229950e0b695b3a60
Karthik Mallina <cloudbykarthikmallina@gmail.com>
2025-12-17 20:45:12 +0530

    Scripting

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# No space befor or after "="
+PERSON1=John
+PERSON2=Raheem
+
+echo "$PERSON1:: Hi $PERSON2, How are You?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: What are you Learning Reacently?"
+echo "$PERSON2:: I am Learning Shell Scripting $PERSON1"
