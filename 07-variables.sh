91096db19782decdb103f4620cd5ad850236809f
Karthik Mallina <cloudbykarthikmallina@gmail.com>
2025-12-18 14:30:01 +0530

    shell script

--- 07-variables.sh
+++ 07-variables.sh
@@ -2,4 +2,12 @@
 
 DATE=$(date)
 
-echo "Timestamp Executed: $DATE"
+START_TIME=$(date +%s)
+
+sleep 10 &
+
+END_TIME=$(date +%s)
+
+TOTAL_TIME=&($END_TIME-$START_TIME)
+
+echo "Script Executed in: $TOTAL_TIME Seconds"
