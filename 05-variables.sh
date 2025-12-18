f0153da3157f98c3fa8f6848c2d288a0d76766c5
Karthik Mallina <cloudbykarthikmallina@gmail.com>
2025-12-18 12:45:38 +0530

    shell script

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Please Enter Your PIN Number"
-read PIN_NUMBER # HERE PIN_NUMBER IS VARIABLE
+read -s PIN_NUMBER # HERE PIN_NUMBER IS VARIABLE
 
 echo "PIN entered is: $PIN_NUMBER"
