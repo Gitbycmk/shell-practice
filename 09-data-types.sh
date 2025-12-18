24728ffe57296356de68c10ec9c9706bc9ff0c82
Karthik Mallina <cloudbykarthikmallina@gmail.com>
2025-12-18 16:58:43 +0530

    shell script

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -13,5 +13,5 @@ echo "SUM is: ${SUM}"
 LEADERS=("Modi" "Putin" "Trudo" "Trump")
 
 echo "All Leaders: ${LEADERS[@]}"
-echo "First Leader: ${LEADER[0]}"
-echo "Second Leader: ${LEADER[10]}"
+echo "First Leader: ${LEADERS[0]}"
+echo "Second Leader: ${LEADERS[10]}"
