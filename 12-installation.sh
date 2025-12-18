4a9647c5e489e5e2f62f83f3bd1e10f67ed1127f
Karthik Mallina <cloudbykarthikmallina@gmail.com>
2025-12-18 19:52:13 +0530

    shell script

---
+++ 12-installation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+USERID=$(ID -U)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please Run This Script with Root Prieledge"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing MySQL is a FAILURE"
+else
+    echo "Installing MySQL is SUCCESS"
+fi
