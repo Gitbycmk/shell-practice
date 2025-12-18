9b18f503ca15dd4eca49bcce3c81584c54ab8052
Karthik Mallina <cloudbykarthikmallina@gmail.com>
2025-12-18 14:51:40 +0530

    shell script

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -3,3 +3,6 @@
 echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
 echo "Script Name: $0"
+echo "Current Working Directory: $PWD"
+echo "Who is Running thisv Scripe: $USER"
+echo "Home Directoryof User: $HOME"
